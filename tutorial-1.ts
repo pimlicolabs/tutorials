e84da71a73633a98c4a91f5f69ad6af5e1b041f9
Kristof Gazso <kristof.gazso@gmail.com>
2024-01-15 15:17:11 +0000

    update tutorials

--- tutorial-1.ts
+++ tutorial-1.ts
@@ -1,148 +1,59 @@
-import dotenv from "dotenv"
-import { UserOperation, bundlerActions, getSenderAddress, signUserOperationHashWithECDSA } from "permissionless"
-import { pimlicoBundlerActions, pimlicoPaymasterActions } from "permissionless/actions/pimlico"
-import { Hex, concat, createClient, createPublicClient, encodeFunctionData, http } from "viem"
-import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
-import { lineaTestnet } from "viem/chains"
-dotenv.config()
+import "dotenv/config"
+import { writeFileSync } from 'fs'
+import { bundlerActions, createSmartAccountClient } from "permissionless"
+import { privateKeyToSafeSmartAccount } from "permissionless/accounts"
+import { pimlicoBundlerActions } from "permissionless/actions/pimlico"
+import { createPimlicoPaymasterClient } from "permissionless/clients/pimlico"
+import { Hex, createPublicClient, http } from "viem"
+import { generatePrivateKey } from "viem/accounts"
+import { sepolia } from "viem/chains"
 
-// CREATE THE CLIENTS
-const publicClient = createPublicClient({
-    transport: http("https://rpc.goerli.linea.build/"),
-    chain: lineaTestnet
+const privateKey = process.env.PRIVATE_KEY as Hex ?? (() => {
+    const pk = generatePrivateKey(); 
+    writeFileSync(".env", `PRIVATE_KEY=${pk}`); 
+    return pk
+})();
+
+const apiKey = process.env.PIMLICO_API_KEY
+if (!apiKey) throw new Error("Missing PIMLICO_API_KEY")
+
+const paymasterUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`
+
+export const publicClient = createPublicClient({
+	transport: http("https://rpc.ankr.com/eth_sepolia"),
 })
 
-const chain = "linea-testnet" // find the list of chain names on the Pimlico verifying paymaster reference page
-const apiKey = process.env.PIMLICO_API_KEY // REPLACE THIS
-
-const bundlerClient = createClient({
-    transport: http(`https://api.pimlico.io/v1/${chain}/rpc?apikey=${apiKey}`),
-    chain: lineaTestnet
-})
-    .extend(bundlerActions)
-    .extend(pimlicoBundlerActions)
-
-const paymasterClient = createClient({
-    // ⚠️ using v2 of the API ⚠️
-    transport: http(`https://api.pimlico.io/v2/${chain}/rpc?apikey=${apiKey}`),
-    chain: lineaTestnet
-}).extend(pimlicoPaymasterActions)
-
-// GENERATE THE INITCODE
-const SIMPLE_ACCOUNT_FACTORY_ADDRESS = "0x9406Cc6185a346906296840746125a0E44976454"
-
-const ownerPrivateKey = generatePrivateKey()
-const owner = privateKeyToAccount(ownerPrivateKey)
-
-console.log("Generated wallet with private key:", ownerPrivateKey)
-
-const initCode = concat([
-    SIMPLE_ACCOUNT_FACTORY_ADDRESS,
-    encodeFunctionData({
-        abi: [
-            {
-                inputs: [
-                    { name: "owner", type: "address" },
-                    { name: "salt", type: "uint256" }
-                ],
-                name: "createAccount",
-                outputs: [{ name: "ret", type: "address" }],
-                stateMutability: "nonpayable",
-                type: "function"
-            }
-        ],
-        args: [owner.address, 0n]
-    })
-])
-
-console.log("Generated initCode:", initCode)
-
-// CALCULATE THE SENDER ADDRESS
-const ENTRY_POINT_ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
-
-const senderAddress = await getSenderAddress(publicClient, {
-    initCode,
-    entryPoint: ENTRY_POINT_ADDRESS
-})
-console.log("Calculated sender address:", senderAddress)
-
-// GENERATE THE CALLDATA
-const to = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045" // vitalik
-const value = 0n
-const data = "0x68656c6c6f" // "hello" encoded to utf-8 bytes
-
-const callData = encodeFunctionData({
-    abi: [
-        {
-            inputs: [
-                { name: "dest", type: "address" },
-                { name: "value", type: "uint256" },
-                { name: "func", type: "bytes" }
-            ],
-            name: "execute",
-            outputs: [],
-            stateMutability: "nonpayable",
-            type: "function"
-        }
-    ],
-    args: [to, value, data]
+export const paymasterClient = createPimlicoPaymasterClient({
+	transport: http(paymasterUrl),
 })
 
-console.log("Generated callData:", callData)
+const account = await privateKeyToSafeSmartAccount(publicClient, {
+	privateKey,
+	safeVersion: "1.4.1", // simple version
+	entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789", // global entrypoint
+});
 
-// FILL OUT REMAINING USER OPERATION VALUES
-const gasPrice = await bundlerClient.getUserOperationGasPrice()
+console.log(`Smart account address: https://sepolia.etherscan.io/address/${account.address}`);
 
-const userOperation = {
-    sender: senderAddress,
-    nonce: 0n,
-    initCode,
-    callData,
-    maxFeePerGas: gasPrice.fast.maxFeePerGas,
-    maxPriorityFeePerGas: gasPrice.fast.maxPriorityFeePerGas,
-    // dummy signature
-    signature:
-        "0xa15569dd8f8324dbeabf8073fdec36d4b754f53ce5901e283c6de79af177dc94557fa3c9922cd7af2a96ca94402d35c39f266925ee6407aeb32b31d76978d4ba1c" as Hex
-}
+const bundlerUrl = `https://api.pimlico.io/v1/sepolia/rpc?apikey=${apiKey}`
 
-// REQUEST PIMLICO VERIFYING PAYMASTER SPONSORSHIP
-const sponsorUserOperationResult = await paymasterClient.sponsorUserOperation({
-    userOperation,
-    entryPoint: ENTRY_POINT_ADDRESS
-})
+const smartAccountClient = createSmartAccountClient({
+	account,
+	chain: sepolia,
+	transport: http(bundlerUrl),
+	sponsorUserOperation: paymasterClient.sponsorUserOperation,
+}).extend(bundlerActions).extend(pimlicoBundlerActions)
 
-const sponsoredUserOperation: UserOperation = {
-    ...userOperation,
-    preVerificationGas: sponsorUserOperationResult.preVerificationGas,
-    verificationGasLimit: sponsorUserOperationResult.verificationGasLimit,
-    callGasLimit: sponsorUserOperationResult.callGasLimit,
-    paymasterAndData: sponsorUserOperationResult.paymasterAndData
-}
+const gasPrices = await smartAccountClient.getUserOperationGasPrice()
 
-console.log("Received paymaster sponsor result:", sponsorUserOperationResult)
+console.log("Received gas prices:", gasPrices)
 
-// SIGN THE USER OPERATION
-const signature = await signUserOperationHashWithECDSA({
-    account: owner,
-    userOperation: sponsoredUserOperation,
-    chainId: lineaTestnet.id,
-    entryPoint: ENTRY_POINT_ADDRESS
-})
-sponsoredUserOperation.signature = signature
+const txHash = await smartAccountClient.sendTransaction({
+	to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
+	value: 0n,
+	data: "0x1234",
+    maxFeePerGas: gasPrices.fast.maxFeePerGas,
+    maxPriorityFeePerGas: gasPrices.fast.maxPriorityFeePerGas,
+});
 
-console.log("Generated signature:", signature)
-
-// SUBMIT THE USER OPERATION TO BE BUNDLED
-const userOperationHash = await bundlerClient.sendUserOperation({
-    userOperation: sponsoredUserOperation,
-    entryPoint: ENTRY_POINT_ADDRESS
-})
-
-console.log("Received User Operation hash:", userOperationHash)
-
-// let's also wait for the userOperation to be included, by continually querying for the receipts
-console.log("Querying for receipts...")
-const receipt = await bundlerClient.waitForUserOperationReceipt({ hash: userOperationHash })
-const txHash = receipt.receipt.transactionHash
-
-console.log(`UserOperation included: https://goerli.lineascan.build/tx/${txHash}`)
+console.log(`UserOperation included: https://sepolia.etherscan.io/tx/${txHash}`)
--- tutorial-2.ts
+++ tutorial-2.ts
@@ -1,41 +1,23 @@
 import dotenv from "dotenv"
-import {
-    UserOperation,
-    bundlerActions,
-    getAccountNonce,
-    getSenderAddress,
-    signUserOperationHashWithECDSA
-} from "permissionless"
+import { UserOperation, bundlerActions, getSenderAddress, signUserOperationHashWithECDSA } from "permissionless"
 import { pimlicoBundlerActions, pimlicoPaymasterActions } from "permissionless/actions/pimlico"
-import { Address, Hash, concat, createClient, createPublicClient, encodeFunctionData, http } from "viem"
-import { privateKeyToAccount } from "viem/accounts"
-import { polygonMumbai } from "viem/chains"
+import { Hex, concat, createClient, createPublicClient, encodeFunctionData, http } from "viem"
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
+import { lineaTestnet } from "viem/chains"
 dotenv.config()
 
-// DEFINE THE CONSTANTS
-const privateKey = "GENERATED_PRIVATE_KEY" // replace this with a private key you generate!
-const apiKey = process.env.PIMLICO_API_KEY // replace with your Pimlico API key
+// CREATE THE CLIENTS
+const publicClient = createPublicClient({
+    transport: http("https://rpc.goerli.linea.build/"),
+    chain: lineaTestnet
+})
 
-const ENTRY_POINT_ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
-const SIMPLE_ACCOUNT_FACTORY_ADDRESS = "0x9406Cc6185a346906296840746125a0E44976454"
-
-const chain = "mumbai"
-
-if (apiKey === undefined) {
-    throw new Error("Please replace the `apiKey` env variable with your Pimlico API key")
-}
-
-if (privateKey.match(/GENERATED_PRIVATE_KEY/)) {
-    throw new Error(
-        "Please replace the `privateKey` variable with a newly generated private key. You can use `generatePrivateKey()` for this"
-    )
-}
-
-const signer = privateKeyToAccount(privateKey as Hash)
+const chain = "linea-testnet" // find the list of chain names on the Pimlico verifying paymaster reference page
+const apiKey = process.env.PIMLICO_API_KEY // REPLACE THIS
 
 const bundlerClient = createClient({
     transport: http(`https://api.pimlico.io/v1/${chain}/rpc?apikey=${apiKey}`),
-    chain: polygonMumbai
+    chain: lineaTestnet
 })
     .extend(bundlerActions)
     .extend(pimlicoBundlerActions)
@@ -43,15 +25,17 @@ const bundlerClient = createClient({
 const paymasterClient = createClient({
     // ⚠️ using v2 of the API ⚠️
     transport: http(`https://api.pimlico.io/v2/${chain}/rpc?apikey=${apiKey}`),
-    chain: polygonMumbai
+    chain: lineaTestnet
 }).extend(pimlicoPaymasterActions)
 
-const publicClient = createPublicClient({
-    transport: http("https://mumbai.rpc.thirdweb.com"),
-    chain: polygonMumbai
-})
+// GENERATE THE INITCODE
+const SIMPLE_ACCOUNT_FACTORY_ADDRESS = "0x9406Cc6185a346906296840746125a0E44976454"
 
-// CALCULATE THE DETERMINISTIC SENDER ADDRESS
+const ownerPrivateKey = generatePrivateKey()
+const owner = privateKeyToAccount(ownerPrivateKey)
+
+console.log("Generated wallet with private key:", ownerPrivateKey)
+
 const initCode = concat([
     SIMPLE_ACCOUNT_FACTORY_ADDRESS,
     encodeFunctionData({
@@ -67,204 +51,98 @@ const initCode = concat([
                 type: "function"
             }
         ],
-        args: [signer.address, 0n]
+        args: [owner.address, 0n]
     })
 ])
+
+console.log("Generated initCode:", initCode)
+
+// CALCULATE THE SENDER ADDRESS
+const ENTRY_POINT_ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
 
 const senderAddress = await getSenderAddress(publicClient, {
     initCode,
     entryPoint: ENTRY_POINT_ADDRESS
 })
-console.log("Counterfactual sender address:", senderAddress)
+console.log("Calculated sender address:", senderAddress)
 
-// DEPLOY THE SIMPLE WALLET
-const genereteApproveCallData = (erc20TokenAddress: Address, paymasterAddress: Address) => {
-    const approveData = encodeFunctionData({
-        abi: [
-            {
-                inputs: [
-                    { name: "_spender", type: "address" },
-                    { name: "_value", type: "uint256" }
-                ],
-                name: "approve",
-                outputs: [{ name: "", type: "bool" }],
-                payable: false,
-                stateMutability: "nonpayable",
-                type: "function"
-            }
-        ],
-        args: [paymasterAddress, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffn]
-    })
+// GENERATE THE CALLDATA
+const to = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045" // vitalik
+const value = 0n
+const data = "0x68656c6c6f" // "hello" encoded to utf-8 bytes
 
-    // GENERATE THE CALLDATA TO APPROVE THE USDC
-    const to = erc20TokenAddress
-    const value = 0n
-    const data = approveData
-
-    const callData = encodeFunctionData({
-        abi: [
-            {
-                inputs: [
-                    { name: "dest", type: "address" },
-                    { name: "value", type: "uint256" },
-                    { name: "func", type: "bytes" }
-                ],
-                name: "execute",
-                outputs: [],
-                stateMutability: "nonpayable",
-                type: "function"
-            }
-        ],
-        args: [to, value, data]
-    })
-
-    return callData
-}
-
-const submitUserOperation = async (userOperation: UserOperation) => {
-    const userOperationHash = await bundlerClient.sendUserOperation({
-        userOperation,
-        entryPoint: ENTRY_POINT_ADDRESS
-    })
-    console.log(`UserOperation submitted. Hash: ${userOperationHash}`)
-
-    console.log("Querying for receipts...")
-    const receipt = await bundlerClient.waitForUserOperationReceipt({
-        hash: userOperationHash
-    })
-    console.log(`Receipt found!\nTransaction hash: ${receipt.receipt.transactionHash}`)
-}
-
-// You can get the paymaster addresses from https://docs.pimlico.io/reference/erc20-paymaster/contracts
-const erc20PaymasterAddress = "0x32aCDFeA07a614E52403d2c1feB747aa8079A353"
-const usdcTokenAddress = "0x0FA8781a83E46826621b3BC094Ea2A0212e71B23" // USDC on Polygon Mumbai
-
-const senderUsdcBalance = await publicClient.readContract({
+const callData = encodeFunctionData({
     abi: [
         {
-            inputs: [{ name: "_owner", type: "address" }],
-            name: "balanceOf",
-            outputs: [{ name: "balance", type: "uint256" }],
-            type: "function",
-            stateMutability: "view"
+            inputs: [
+                { name: "dest", type: "address" },
+                { name: "value", type: "uint256" },
+                { name: "func", type: "bytes" }
+            ],
+            name: "execute",
+            outputs: [],
+            stateMutability: "nonpayable",
+            type: "function"
         }
     ],
-    address: usdcTokenAddress,
-    functionName: "balanceOf",
-    args: [senderAddress]
+    args: [to, value, data]
 })
 
-if (senderUsdcBalance < 1_000_000n) {
-    throw new Error(
-        `insufficient USDC balance for counterfactual wallet address ${senderAddress}: ${
-            Number(senderUsdcBalance) / 1000000
-        } USDC, required at least 1 USDC`
-    )
-}
+console.log("Generated callData:", callData)
 
-const approveCallData = genereteApproveCallData(usdcTokenAddress, erc20PaymasterAddress)
+// FILL OUT REMAINING USER OPERATION VALUES
+const gasPrice = await bundlerClient.getUserOperationGasPrice()
 
-// FILL OUT THE REMAINING USEROPERATION VALUES
-const gasPriceResult = await bundlerClient.getUserOperationGasPrice()
-
-const userOperation: Partial<UserOperation> = {
+const userOperation = {
     sender: senderAddress,
     nonce: 0n,
     initCode,
-    callData: approveCallData,
-    maxFeePerGas: gasPriceResult.fast.maxFeePerGas,
-    maxPriorityFeePerGas: gasPriceResult.fast.maxPriorityFeePerGas,
-    paymasterAndData: "0x",
+    callData,
+    maxFeePerGas: gasPrice.fast.maxFeePerGas,
+    maxPriorityFeePerGas: gasPrice.fast.maxPriorityFeePerGas,
+    // dummy signature
     signature:
-        "0xfffffffffffffffffffffffffffffff0000000000000000000000000000000007aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1c"
+        "0xa15569dd8f8324dbeabf8073fdec36d4b754f53ce5901e283c6de79af177dc94557fa3c9922cd7af2a96ca94402d35c39f266925ee6407aeb32b31d76978d4ba1c" as Hex
 }
 
-const nonce = await getAccountNonce(publicClient, {
-    entryPoint: ENTRY_POINT_ADDRESS,
-    sender: senderAddress
-})
-
-if (nonce === 0n) {
-    // SPONSOR THE USEROPERATION USING THE VERIFYING PAYMASTER
-    const result = await paymasterClient.sponsorUserOperation({
-        userOperation: userOperation as UserOperation,
-        entryPoint: ENTRY_POINT_ADDRESS
-    })
-
-    userOperation.preVerificationGas = result.preVerificationGas
-    userOperation.verificationGasLimit = result.verificationGasLimit
-    userOperation.callGasLimit = result.callGasLimit
-    userOperation.paymasterAndData = result.paymasterAndData
-
-    // SIGN THE USEROPERATION
-    const signature = await signUserOperationHashWithECDSA({
-        account: signer,
-        userOperation: userOperation as UserOperation,
-        chainId: polygonMumbai.id,
-        entryPoint: ENTRY_POINT_ADDRESS
-    })
-
-    userOperation.signature = signature
-    await submitUserOperation(userOperation as UserOperation)
-} else {
-    console.log("Deployment UserOperation previously submitted, skipping...")
-}
-
-// SPONSOR A USER OPERATION WITH THE ERC-20 PAYMASTER
-const genereteDummyCallData = () => {
-    // SEND EMPTY CALL TO VITALIK
-    const to = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045" // vitalik
-    const value = 0n
-    const data = "0x"
-
-    const callData = encodeFunctionData({
-        abi: [
-            {
-                inputs: [
-                    { name: "dest", type: "address" },
-                    { name: "value", type: "uint256" },
-                    { name: "func", type: "bytes" }
-                ],
-                name: "execute",
-                outputs: [],
-                stateMutability: "nonpayable",
-                type: "function"
-            }
-        ],
-        args: [to, value, data]
-    })
-
-    return callData
-}
-
-console.log("Sponsoring a user operation with the ERC-20 paymaster...")
-
-const newNonce = await getAccountNonce(publicClient, {
-    entryPoint: ENTRY_POINT_ADDRESS,
-    sender: senderAddress
-})
-
-const sponsoredUserOperation: UserOperation = {
-    sender: senderAddress,
-    nonce: newNonce,
-    initCode: "0x",
-    callData: genereteDummyCallData(),
-    callGasLimit: 100_000n, // hardcode it for now at a high value
-    verificationGasLimit: 500_000n, // hardcode it for now at a high value
-    preVerificationGas: 50_000n, // hardcode it for now at a high value
-    maxFeePerGas: gasPriceResult.fast.maxFeePerGas,
-    maxPriorityFeePerGas: gasPriceResult.fast.maxPriorityFeePerGas,
-    paymasterAndData: erc20PaymasterAddress, // to use the erc20 paymaster, put its address in the paymasterAndData field
-    signature: "0x"
-}
-
-// SIGN THE USEROPERATION
-
-sponsoredUserOperation.signature = await signUserOperationHashWithECDSA({
-    account: signer,
-    userOperation: sponsoredUserOperation,
-    chainId: polygonMumbai.id,
+// REQUEST PIMLICO VERIFYING PAYMASTER SPONSORSHIP
+const sponsorUserOperationResult = await paymasterClient.sponsorUserOperation({
+    userOperation,
     entryPoint: ENTRY_POINT_ADDRESS
 })
 
-await submitUserOperation(sponsoredUserOperation)
+const sponsoredUserOperation: UserOperation = {
+    ...userOperation,
+    preVerificationGas: sponsorUserOperationResult.preVerificationGas,
+    verificationGasLimit: sponsorUserOperationResult.verificationGasLimit,
+    callGasLimit: sponsorUserOperationResult.callGasLimit,
+    paymasterAndData: sponsorUserOperationResult.paymasterAndData
+}
+
+console.log("Received paymaster sponsor result:", sponsorUserOperationResult)
+
+// SIGN THE USER OPERATION
+const signature = await signUserOperationHashWithECDSA({
+    account: owner,
+    userOperation: sponsoredUserOperation,
+    chainId: lineaTestnet.id,
+    entryPoint: ENTRY_POINT_ADDRESS
+})
+sponsoredUserOperation.signature = signature
+
+console.log("Generated signature:", signature)
+
+// SUBMIT THE USER OPERATION TO BE BUNDLED
+const userOperationHash = await bundlerClient.sendUserOperation({
+    userOperation: sponsoredUserOperation,
+    entryPoint: ENTRY_POINT_ADDRESS
+})
+
+console.log("Received User Operation hash:", userOperationHash)
+
+// let's also wait for the userOperation to be included, by continually querying for the receipts
+console.log("Querying for receipts...")
+const receipt = await bundlerClient.waitForUserOperationReceipt({ hash: userOperationHash })
+const txHash = receipt.receipt.transactionHash
+
+console.log(`UserOperation included: https://goerli.lineascan.build/tx/${txHash}`)
